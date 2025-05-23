8f7653fc83280da894bddd590d6f0eea926c3df5
Amaan-Multani <multaniamaan885@gmail.com>
2025-05-23 17:49:57 +0530

    Add files via upload

---
+++ MathDemo.java
@@ -0,0 +1,14 @@
+class MathDemo
+{
+	public static void main (String args [])
+	{
+		int a[] = new int [15];
+		int i;
+		
+		for (i=0;i<15;i++)
+		{
+			a[i] = (int)(Math.random()*50);
+			System.out.println(a[i]);
+		}
+}
+}
